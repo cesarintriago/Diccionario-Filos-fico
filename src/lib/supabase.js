7d5de689af2e09b0b117870388a870222df5885e
cesarintriago <cesar.intriago@hotmail.com>
2024-05-06 14:38:55 +0200

    "YA HAY CONEXIÓN CON SUPABASE"

---
+++ src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://dnjjrgjhlvfdkiisgqqc.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuampyZ2pobHZmZGtpaXNncXFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3NDMwMjEsImV4cCI6MjAzMDMxOTAyMX0.ZRCrA65sUxwsrWZyBkhjoEgSLLKbcZMkxlwhWeckMQk');
+
