a4a3d569bbb3134e9f1ad8b127e49877ad9b4dde
M1steriosoc <A01366738@tec.mx>
2025-06-06 19:33:15 -0600

    Update ontology pages and content structure
    - Changed page titles for 'Blog' to more specific titles such as 'Ontología', 'Cuestiones preambulares', 'Apariencia y Verdad', 'Causalidad', 'Idea general y Ontología de la Corrupción', 'Dialéctica', 'Existencia, Posibilidad, Necesidad', and 'Finalidad'.
    - Updated Open Graph image link and text content in the default image generation script.
    - Added new Astro pages for various ontology topics, including dynamic routing based on article IDs and slugs.
    - Implemented data fetching from Supabase for articles based on categories and IDs.
    - Enhanced layout structure for better content presentation across different ontology topics.
    - Introduced loading states for article content while data is being fetched.
    - Added styles and components for improved UI consistency.

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -13,19 +13,19 @@ const height = 630;
 const width = 1200;
 
 export const GET: APIRoute = async () => {
-  const link = 'https://brutal.elian.codes';
+  const link = 'https://filosofia.cesarintriago.net';
   const html = toReactElement(`
   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Brutal</p>
-          <p style="font-size: 38px;">A theme for Astro</p>
-          <p style="font-size: 38px;">Brought to you by Elian</p>
+          <p style="font-size: 48px;">Materialismo Filosófico</p>
+          <p style="font-size: 38px;">Sitio web del sistema</p>
+          <p style="font-size: 38px;">Hecho por César Intriago, basado en el Diccionario de Pelayo García Sierra y en la obra de Gustavo Bueno</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://64.media.tumblr.com/e70c035af5be2789c697544ad1c16c7a/7e554d6d30914750-9d/s1280x1920/3745dec19f39427b5e701ebf781c7ee540f5de1f.pnj" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
